429920f70a5e4160fc42a90a80b883fefced18b2
Didar Yermekuly <yermekulyd@gmail.com>
2024-09-11 13:34:52 +0500

    global: adding globals.css file

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,4 @@ import { RouterView } from 'vue-router'
   </div>
 </template>
 
-<style scoped>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
-</style>
+<style scoped></style>
